b9a02465fb0e0e4c9ee9189bf2b26de70cd62b49
CodeDad <github email address>
2018-07-26 22:29:47 -0400

    created a home controller with an index and set root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: 'site#home'
+
   namespace :api do 
     namespace :v1 do
-      resources :fruits, only [:index, :create, :update, :destroy]
+      resources :fruits, only: [:index, :create, :update, :destroy]
     end
-  end  
+  end
+    
 end
 
 
